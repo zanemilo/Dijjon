b4be0f0e87858354c31f147fcc23980c80cd2c30
Zane Deso <zanemilo@yahoo.com>
2024-02-02 16:07:27 -0500

    fixed another error with the previous implemented methods. all fixed

--- new main.py
+++ new main.py
@@ -26,7 +26,7 @@ print(f'Passed dex Check?: {master.check("dex", player.player_check_roll("dex"))
 print(f'Passed int Check?: {master.check("int", player.player_check_roll("int"))}') # example int check
 print(f'Passed arcana Check?: {master.check("arcana", player.player_check_roll("arcana"))}') # example arcana check
 print(f'Passed investigation Check?: {master.check("investigation", player.player_check_roll("investigation"))}') # example investigation check
-print(f'Passed  Check?: {master.check("sleight_of_hand", player.player_check_roll("sleight_of_hand"))}') # example sleight_of_hand check
+print(f'Passed sleight of hand Check?: {master.check("sleight_of_hand", player.player_check_roll("sleight_of_hand"))}') # example sleight_of_hand check
 
 while main_game_loop:
 
